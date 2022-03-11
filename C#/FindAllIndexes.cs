fd3dbb08ec2d52cc656d076b3e7c6d86b9a4c8b9
Brandon <brandon.dicicco@gmail.com>
2022-03-11 18:04:49 -0500

    Add code sample

---
+++ C#/FindAllIndexes.cs
@@ -0,0 +1,11 @@
+//Find All indexes for a substring in String
+int index = 0;
+string message = "This is a string of string of string."
+string searchStr =  "string"
+List<int> indexes = new List<int>
+while ((index = message.IndexOf(searchStr, index)) != -1)
+{
+    indexes.add(index);
+    index++;
+}
+}
