1097d140d5b364a98a60795c6e6b6aecde95c715
Brandon <brandon.dicicco@gmail.com>
2022-03-12 01:30:56 -0500

    missed semicolon

--- C#/FindAllIndexes.cs
+++ C#/FindAllIndexes.cs
@@ -2,7 +2,8 @@
 int index = 0;
 string message = "This is a string of string of string."
 string searchStr =  "string"
-List<int> indexes = new List<int>
+List<int> indexes = new List<int>;
+
 while ((index = message.IndexOf(searchStr, index)) != -1)
 {
     indexes.add(index);
