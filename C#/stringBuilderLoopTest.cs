86f51be9ef24023ece9026c19f12f08bfbe0969d
Brandon <brandon.dicicco@gmail.com>
2022-03-16 21:49:01 -0400

    add string param

--- C#/stringBuilderLoopTest.cs
+++ C#/stringBuilderLoopTest.cs
@@ -1,5 +1,6 @@
 public string StringBuilderTest()
 {
+    string message = "Test Message";
     string prefix = "";
     StringBuilder sb = new StringBuilder();
     for (int i = 0; i < message.Length; i++)
